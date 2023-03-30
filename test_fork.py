bc71ab2e4f69f1251170645a49204606b9ec6fc2
Masahiro Masuda <masahi129@gmail.com>
2023-03-30 17:30:36 +0900

    wip

--- test_fork.py
+++ test_fork.py
@@ -1,6 +1,7 @@
 import tvm
 from tvm.script import relax as R
 from tvm.relax.dpl import *
+from tvm import relax
 
 
 def test_CBR_x2():
@@ -79,9 +80,9 @@ def test_qkv():
         K_weight_pat = wildcard()
         V_weight_pat = wildcard()
 
-        is_op("relax.matmul")(inp_pat, Q_weight_pat)
-        is_op("relax.matmul")(inp_pat, K_weight_pat)
-        is_op("relax.matmul")(inp_pat, V_weight_pat)
+        matmul1 = is_op("relax.matmul")(inp_pat, Q_weight_pat)
+        matmul2 = is_op("relax.matmul")(inp_pat, K_weight_pat)
+        matmul3 = is_op("relax.matmul")(inp_pat, V_weight_pat)
 
         dfb = QKV_proj["main"].body.blocks[0]
         out = ctx.match_dfb(dfb)
@@ -89,6 +90,43 @@ def test_qkv():
         assert out[Q_weight_pat].name_hint == "w0"
         assert out[K_weight_pat].name_hint == "w1"
         assert out[V_weight_pat].name_hint == "w2"
+
+        def rewriter(builder, matchings):
+            inp = matchings[inp_pat]
+            Q_weight = matchings[Q_weight_pat]
+            K_weight = matchings[K_weight_pat]
+            V_weight = matchings[V_weight_pat]
+            width = Q_weight.struct_info.shape[1]
+
+            concat = R.concat([Q_weight, K_weight, V_weight], axis=1)
+            matmul = R.matmul(inp, concat)
+            Q = builder.normalize(R.strided_slice(matmul, axes=[2], begin=[0], end=[width]))
+            K = builder.normalize(R.strided_slice(matmul, axes=[2], begin=[width], end=[width*2]))
+            V = builder.normalize(R.strided_slice(matmul, axes=[2], begin=[width*2], end=[width*3]))
+
+            print(Q.struct_info)
+
+            Q_var = relax.Var("Q")
+            K_var = relax.Var("K")
+            V_var = relax.Var("V")
+
+            return {matchings[matmul1]: Q,
+                    matchings[matmul2]: K,
+                    matchings[matmul3]: V}
+
+            # dfb_writer.add(concat, "concat")
+            # dfb_writer.add(matmul, "matmul")
+
+            # dfb_writer.add(relax.VarBinding(Q_var, Q))
+            # dfb_writer.add(relax.VarBinding(K_var, K))
+            # dfb_writer.add(relax.VarBinding(V_var, V))
+
+            # dfb.replace_all_uses(matchings[matmul1], Q_var)
+            # dfb.replace_all_uses(matchings[matmul2], K_var)
+            # dfb.replace_all_uses(matchings[matmul3], V_var)
+
+        print(rewrite_bindings(ctx, rewriter, QKV_proj["main"]))
+
 
 
 def test_CBR_x2_op():
@@ -137,9 +175,9 @@ def test_CBR_x2_op():
         dfb = CBRx2["main"].body.blocks[0]
         out = ctx.match_dfb(dfb)
 
-        print(out)
+        print(out[relu1])
         # print(out[weight1])
 
 
 test_qkv()
-# test_CBR_x2()
+# test_CBR_x2_op()
