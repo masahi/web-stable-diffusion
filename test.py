23ea19dfea3894dd0a7c53979485f65cf5b465d9
Masahiro Masuda <masahi129@gmail.com>
2023-04-11 20:25:05 +0900

    use CombineParallelMatmul pass

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from tvm.relax.backend.contrib.cutlass import partition_for_cutlass
 from tvm.relax.dpl import rewrite_call, rewrite_bindings, is_op, wildcard, is_const, PatternContext
 from tvm.script import relax as R
 from tvm.relax.transform import ConvertLayout, Normalize, ToMixedPrecision, EliminateCommonSubexpr, CanonicalizeBindings
+from tvm.relax.transform import CombineParallelMatmul
 
 
 def rewrite_attention(f):
@@ -34,43 +35,6 @@ def rewrite_attention(f):
     return rewrite_call(pattern, callback, f)
 
 
-def combine_parallel_matmul(f, num_branches, slice_axis=2):
-    with PatternContext() as ctx:
-        inp_pat = wildcard()
-
-        weight_patterns = []
-        matmul_patterns = []
-
-        for _ in range(num_branches):
-            w_pat = wildcard()
-            weight_patterns.append(w_pat)
-            matmul_patterns.append(is_op("relax.matmul")(inp_pat, w_pat))
-
-    def rewriter(matchings):
-        inp = matchings[inp_pat]
-
-        weights = [matchings[w_pat] for w_pat in weight_patterns]
-        concat = R.concat(weights, axis=1)
-        matmul = R.matmul(inp, concat)
-
-        begin = 0
-        replacements = {}
-
-        for i, matmul_pat in enumerate(matmul_patterns):
-            width = weights[i].struct_info.shape[1]
-            bound_var = matchings[matmul_pat]
-            replacements[bound_var] = R.strided_slice(matmul, axes=[slice_axis], begin=[begin], end=[begin + width])
-            begin += width
-
-        return replacements
-
-    return rewrite_bindings(ctx, rewriter, f)
-
-
-def rewrite_qkv_proj(f):
-    return combine_parallel_matmul(f, 3)
-
-
 def simplify_div(f):
     lhs_pat = wildcard()
     rhs_pat = is_const()
@@ -97,9 +61,12 @@ mod["main"] = mod_tmp["unet"]
 mod = EliminateCommonSubexpr()(mod)
 mod = CanonicalizeBindings()(mod)
 mod["main"] = rewrite_attention(mod["main"])
-mod["main"] = combine_parallel_matmul(mod["main"], 32)
-mod["main"] = combine_parallel_matmul(mod["main"], 22, slice_axis=1)
-mod["main"] = rewrite_qkv_proj(mod["main"])
+import time
+print("combine parallel matmul")
+t1 = time.time()
+mod = CombineParallelMatmul()(mod)
+t2 = time.time()
+print("done", t2 - t1)
 mod["main"] = simplify_div(mod["main"])
 mod = ConvertLayout({"relax.nn.conv2d": ["NHWC", "OHWI"]})(mod)
 # mod = ToMixedPrecision(out_dtype="float16")(mod)
