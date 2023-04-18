5838ba505ced13a1283df63cd4f07412194f40c1
Masahiro Masuda <masahi129@gmail.com>
2023-04-18 10:09:41 +0900

    wip

--- compile.py
+++ compile.py
@@ -3,7 +3,7 @@ import numpy as np
 import tvm
 from tvm import relax, tir
 from tvm.relax.backend.contrib.cutlass import partition_for_cutlass
-from tvm.relax.dpl import rewrite_call, is_op, wildcard, is_const, PatternContext, rewrite_bindings
+from tvm.relax.dpl import rewrite_call, is_op, wildcard, is_const
 from tvm.script import relax as R
 
 
@@ -65,95 +65,19 @@ def simplify_div(f):
     return rewrite_call(pattern, callback, f)
 
 
-def rewrite_qkv_proj(f):
-    def matmul_transposed_reshape(tensor, weight_transposed):
-        return is_op("relax.reshape")(is_op("relax.matmul")(tensor, weight_transposed), wildcard())
-
-    inp_pat = wildcard()
-    Q_weight_pat = wildcard()
-    K_weight_pat = wildcard()
-    V_weight_pat = wildcard()
-
-    Q = matmul_transposed_reshape(inp_pat, Q_weight_pat)
-    K = matmul_transposed_reshape(inp_pat, K_weight_pat)
-    V = matmul_transposed_reshape(inp_pat, V_weight_pat)
-
-    pattern = is_op("relax.nn.attention")(Q, K, V)
-
-    def rewriter(_, matchings):
-        inp = matchings[inp_pat]
-        Q_weight = matchings[Q_weight_pat]
-        K_weight = matchings[K_weight_pat]
-        V_weight = matchings[V_weight_pat]
-
-        width = Q_weight.struct_info.shape[1]
-        weight_concat = R.concat([Q_weight, K_weight, V_weight], axis=1)
-        matmul = R.matmul(inp, weight_concat)
-        Q = R.strided_slice(matmul, axes=[2], begin=[0], end=[width])
-        K = R.strided_slice(matmul, axes=[2], begin=[width], end=[width*2])
-        V = R.strided_slice(matmul, axes=[2], begin=[width*2], end=[width*3])
-
-        num_head = 8
-        hidden = width // num_head
-        seq_len = inp.struct_info.shape[1]
-
-        Q = R.reshape(Q, R.shape([2, seq_len, num_head, hidden]))
-        K = R.reshape(K, R.shape([2, seq_len, num_head, hidden]))
-        V = R.reshape(V, R.shape([2, seq_len, num_head, hidden]))
-
-        return R.nn.attention(Q, K, V)
-
-    return rewrite_call(pattern, rewriter, f)
-
-
-def combine_parallel_matmul(f, num_branches, slice_axis=2):
-    with PatternContext() as ctx:
-        inp_pat = wildcard()
-
-        weight_patterns = []
-        matmul_patterns = []
-
-        for _ in range(num_branches):
-            w_pat = wildcard()
-            weight_patterns.append(w_pat)
-            matmul_patterns.append(is_op("relax.matmul")(inp_pat, w_pat))
-
-    def rewriter(matchings):
-        inp = matchings[inp_pat]
-
-        weights = [matchings[w_pat] for w_pat in weight_patterns]
-        concat = R.concat(weights, axis=1)
-        matmul = R.matmul(inp, concat)
-
-        begin = 0
-        replacements = {}
-
-        sections = []
-        ind = 0
-        for i, matmul_pat in enumerate(matmul_patterns[:-1]):
-            width = weights[i].struct_info.shape[1]
-            ind += width
-            sections.append(int(ind))
-
-        chunks = R.split(matmul, sections, slice_axis)
-
-        for i, matmul_pat in enumerate(matmul_patterns):
-            bound_var = matchings[matmul_pat]
-            replacements[bound_var] = chunks[i]
-
-        return replacements
-
-    return rewrite_bindings(ctx, rewriter, f)
-
-
 def run_opt_passes(mod):
     return tvm.transform.Sequential(
         [
-            # relax.transform.EliminateCommonSubexpr(),
-            # relax.transform.CanonicalizeBindings(),
-            # relax.transform.CombineParallelMatmul(),
+            relax.transform.EliminateCommonSubexpr(),
+            relax.transform.CanonicalizeBindings(),
+            relax.transform.CombineParallelMatmul(),
             relax.transform.ConvertLayout({"relax.nn.conv2d": ["NHWC", "OHWI"]}),
             relax.transform.ToMixedPrecision(out_dtype="float16"),
+            # relax.transform.LegalizeOps(),
+            # relax.transform.AnnotateTIROpPattern(),
+            # relax.transform.FoldConstant(),
+            # relax.transform.FuseOps(),
+            # relax.transform.FuseTIR(),
         ]
     )(mod)
 
@@ -169,10 +93,11 @@ def get_lower_passes(params):
 
 def get_result(mod, target, dev, inputs, time_eval=False):
     ex = relax.build(mod, target)
-    vm = relax.VirtualMachine(ex, dev)
+    vm = relax.VirtualMachine(ex, dev, profile=True)
     out = vm["main"](*inputs)
 
     if time_eval:
+        print(vm.profile("main", *inputs))
         vm.set_input("main", *inputs)
         print(vm.time_evaluator("invoke_stateful", dev, repeat=50)("main"))
 
@@ -192,39 +117,32 @@ def get_ref(mod, params, target, dev, inputs):
 
     return get_result(mod, target, dev, inputs)
 
+
+target = tvm.target.Target("nvidia/geforce-rtx-3070")
+
+dev = tvm.device("cuda", 0)
+inp_0 = tvm.nd.array(np.random.randn(1, 4, 64, 64).astype("float32"), dev)
+inp_1 = tvm.nd.array(np.array(1, "int32"), dev)
+inp_2 = tvm.nd.array(np.random.randn(2, 77, 768).astype("float32"), dev)
+inputs = [inp_0, inp_1, inp_2]
+
 mod, params = deserialize("unet")
 
 mod["main"] = rewrite_attention(mod["main"])
 mod["main"] = simplify_div(mod["main"])
 
-mod = relax.transform.EliminateCommonSubexpr()(mod)
-mod = relax.transform.CanonicalizeBindings()(mod)
-mod = relax.transform.CombineParallelMatmul()(mod)
+ref = get_ref(mod, params, target, dev, inputs)
 
-# mod["main"] = combine_parallel_matmul(mod["main"], 32)
-# mod["main"] = combine_parallel_matmul(mod["main"], 22, slice_axis=1)
-# mod["main"] = combine_parallel_matmul(mod["main"], 3)
-# mod["main"] = combine_parallel_matmul(mod["main"], 2)
+mod = run_opt_passes(mod)
 
-print(mod)
+mod = partition_for_cutlass(mod)
+# print(mod)
+mod = relax.transform.RunCodegen({"cutlass": {"sm": 80, "find_first_valid": True}})(mod)
 
-# mod = run_opt_passes(mod)
+with target:
+    mod = get_lower_passes(params)(mod)
 
-# mod = partition_for_cutlass(mod)
-# mod = relax.transform.RunCodegen({"cutlass": {"sm": 80, "find_first_valid": True}})(mod)
+out = get_result(mod, target, dev, inputs, time_eval=True)
 
-# target = tvm.target.Target("nvidia/geforce-rtx-3070")
-# with target:
-#     mod = get_lower_passes(params)(mod)
-
-# dev = tvm.device("cuda", 0)
-# inp_0 = tvm.nd.array(np.random.randn(1, 4, 64, 64).astype("float32"), dev)
-# inp_1 = tvm.nd.array(np.array(1, "int32"), dev)
-# inp_2 = tvm.nd.array(np.random.randn(2, 77, 768).astype("float32"), dev)
-# inputs = [inp_0, inp_1, inp_2]
-
-# ref = get_ref(mod, params, target, dev, inputs)
-# out = get_result(mod, target, dev, inputs, time_eval=False)
-
-# print(np.max(np.abs(out - ref)), np.mean(np.abs(out - ref)))
-# print(out)
+print(np.max(np.abs(out - ref)), np.mean(np.abs(out - ref)))
+# # print(out)
