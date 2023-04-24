2cb416d81544dd5ad75a882588d59fab79c59b67
Masahiro Masuda <masahi129@gmail.com>
2023-04-24 15:01:04 +0900

    wip

--- compile.py
+++ compile.py
@@ -66,43 +66,53 @@ def simplify_div(f):
     return rewrite_call(pattern, callback, f)
 
 
-def run_opt_passes(mod):
+def run_opt_passes(mod, params=None):
+    if params:
+        return tvm.transform.Sequential(
+            [
+                relax.transform.ConvertLayout({"relax.nn.conv2d": ["NHWC", "OHWI"]}),
+                relax.transform.BindParams("main", params),
+                relax.transform.FoldConstant(),
+                relax.transform.ToMixedPrecision(out_dtype="float16"),
+            ]
+        )(mod)
+
     return tvm.transform.Sequential(
-        [
-            # relax.transform.EliminateCommonSubexpr(),
-            # relax.transform.CanonicalizeBindings(),
-            # relax.transform.CombineParallelMatmul(),
-            relax.transform.ConvertLayout({"relax.nn.conv2d": ["NHWC", "OHWI"]}),
-            relax.transform.BindParams("main", params),
-            relax.transform.FoldConstant(),
-            relax.transform.ToMixedPrecision(out_dtype="float16"),
-        ]
-    )(mod)
+            [
+                relax.transform.ConvertLayout({"relax.nn.conv2d": ["NHWC", "OHWI"]}),
+                relax.transform.ToMixedPrecision(out_dtype="float16"),
+            ]
+        )(mod)
 
 
 def run_lower_passes(mod, target, tune=False):
     passes = [relax.pipeline.get_pipeline()]
 
-    if not tune:
-        passes.append(tir.transform.DefaultGPUSchedule())
-    else:
-        work_dir = "work"
-        with target:
-            # passes.append(relax.transform.MetaScheduleTuneIRMod(
-            #     params={},
-            #     work_dir=work_dir,
-            #     max_trials_global=500,
-            # ))
-            passes.append(relax.transform.MetaScheduleApplyDatabase(work_dir))
+    if "cuda" in target.kind.name:
+        if not tune:
             passes.append(tir.transform.DefaultGPUSchedule())
+        else:
+            work_dir = "work"
+            with target:
+                # passes.append(relax.transform.MetaScheduleTuneIRMod(
+                #     params={},
+                #     work_dir=work_dir,
+                #     max_trials_global=500,
+                # ))
+                passes.append(relax.transform.MetaScheduleApplyDatabase(work_dir))
+                passes.append(tir.transform.DefaultGPUSchedule())
 
     # with target, tvm.transform.PassContext(trace=Trace(mod), opt_level=0):
     with target, tvm.transform.PassContext(opt_level=3):
         return tvm.transform.Sequential(passes)(mod)
 
 
-def get_result(ex, dev, inputs, time_eval=False):
+def get_result(ex, dev, inputs, params=None, time_eval=False):
     vm = relax.VirtualMachine(ex, dev, profile=True)
+
+    # if params:
+    #     inputs.append(params.values())
+
     out = vm["main"](*inputs)
 
     if time_eval:
@@ -113,24 +123,37 @@ def get_result(ex, dev, inputs, time_eval=False):
     return out.numpy()
 
 
-def get_ref(mod, params, target, dev, inputs):
-    mod = tvm.transform.Sequential(
-        [
-            relax.transform.BindParams("main", params),
-            relax.transform.FoldConstant(),
-            relax.transform.ToMixedPrecision(out_dtype="float16"),
-        ]
-    )(mod)
+def get_ref(mod, params, target, dev, inputs, bind_params=True):
+    passes = []
+
+    if bind_params:
+        passes += [relax.transform.BindParams("main", params),
+                   relax.transform.FoldConstant()]
+
+    passes.append(relax.transform.ToMixedPrecision(out_dtype="float16"))
+
+    mod = tvm.transform.Sequential(passes)(mod)
 
     mod = run_lower_passes(mod, target)
     ex = relax.build(mod, target)
-    return get_result(ex, dev, inputs)
+
+    if bind_params:
+        return get_result(ex, dev, inputs)
+
+    return get_result(ex, dev, inputs, params=params)
 
 
-model = "unet"
-so_name = "{}.so".format(model)
+bind_params = True
 
-target = tvm.target.Target("nvidia/geforce-rtx-3070")
+model = "clip"
+
+if bind_params:
+    so_name = "{}.so".format(model)
+else:
+    so_name = "{}_no_params.so".format(model)
+
+# target = tvm.target.Target("nvidia/geforce-rtx-3070")
+target = tvm.target.Target("llvm")
 
 dev = tvm.device(target.kind.name, 0)
 inp_0 = tvm.nd.array(np.random.randn(1, 4, 64, 64).astype("float32"), dev)
@@ -150,21 +173,27 @@ else:
 
 mod, params = deserialize(model)
 
-ref = get_ref(mod, params, target, dev, inputs)
+ref = get_ref(mod, params, target, dev, inputs, bind_params=bind_params)
 
 mod["main"] = rewrite_attention(mod["main"])
 mod["main"] = simplify_div(mod["main"])
 
-mod = run_opt_passes(mod)
+if bind_params:
+    mod = run_opt_passes(mod, params)
+else:
+    mod = run_opt_passes(mod)
 
-mod = partition_for_cutlass(mod)
-mod = relax.transform.RunCodegen({"cutlass": {"sm": 80, "find_first_valid": True}})(mod)
+# mod = partition_for_cutlass(mod)
+# mod = relax.transform.RunCodegen({"cutlass": {"sm": 80, "find_first_valid": True}})(mod)
 
 mod = run_lower_passes(mod, target, tune=True)
 
 ex = relax.build(mod, target)
 ex.export_library(so_name)
 
-out = get_result(ex, dev, inputs, time_eval=False)
+if bind_params:
+    out = get_result(ex, dev, inputs, time_eval=False)
+else:
+    out = get_result(ex, dev, inputs, params=params, time_eval=False)
 
 print(np.max(np.abs(out - ref)), np.mean(np.abs(out - ref)))
